b4b20fc19c631e6e0fb71afa02a642b7f592cd6f
Sohyun-Dev <jf32430413@gmail.com>
2021-01-24 13:25:29 +0900

    homework2 수정

--- src/tutoring02/homework/MovieTheater.java
+++ src/tutoring02/homework/MovieTheater.java
@@ -3,6 +3,5 @@ package tutoring02.homework;
 public class MovieTheater {
     public static void main(String[] args) {
         Seller seller = new Seller();
-        Watcher watcher = new Watcher();
     }
 }
